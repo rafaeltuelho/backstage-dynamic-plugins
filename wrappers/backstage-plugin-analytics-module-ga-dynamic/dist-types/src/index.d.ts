78a3e12f1dd4815be78cc7db855d8f517a529376
Rafael T. C. Soares <rafaelcba@gmail.com>
2024-03-07 14:14:58 -0600

    GA v4

--- wrappers/backstage-plugin-analytics-module-ga-dynamic/dist-types/src/index.d.ts
+++ wrappers/backstage-plugin-analytics-module-ga-dynamic/dist-types/src/index.d.ts
@@ -1,5 +1,5 @@
-import { GoogleAnalytics } from '@backstage/plugin-analytics-module-ga';
-declare const GoogleAnalyticsApi: import("@backstage/core-plugin-api").ApiFactory<import("@backstage/core-plugin-api").AnalyticsApi, GoogleAnalytics, {
+import { GoogleAnalytics4 } from '@backstage/plugin-analytics-module-ga4';
+declare const GoogleAnalyticsApi: import("@backstage/core-plugin-api").ApiFactory<import("@backstage/core-plugin-api").AnalyticsApi, GoogleAnalytics4, {
     configApi: import("@backstage/config").Config;
     identityApi: import("@backstage/core-plugin-api").IdentityApi;
 }>;
